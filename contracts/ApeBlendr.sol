56302d034a8b6f8285b109369ee173e8fd480e53
strenev <stan@0xfusion.com>
2023-10-10 09:32:03 +0300

    feat: Implement VRFv2Consumer.sol from Chainlink

--- contracts/ApeBlendr.sol
+++ contracts/ApeBlendr.sol
@@ -2,9 +2,27 @@
 pragma solidity 0.8.21;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {VRFv2Consumer} from "./chainlink/VRFv2Consumer.sol";
 
 import {IApeCoinStaking} from "./interfaces/IApeCoinStaking.sol";
 
-contract ApeBlendr is ERC20 {
-    constructor() ERC20("ApeBlendr", "APEd") {}
+contract ApeBlendr is ERC20, VRFv2Consumer {
+    constructor(
+        uint64 _subscriptionId,
+        bytes32 _keyHash,
+        uint32 _callbackGasLimit,
+        uint16 _requestConfirmations,
+        uint32 _numWords,
+        address _vrfCordinator
+    )
+        ERC20("ApeBlendr", "APEd")
+        VRFv2Consumer(_subscriptionId, _keyHash, _callbackGasLimit, _requestConfirmations, _numWords, _vrfCordinator)
+    {}
+
+    function requestRandomWords() internal returns (uint256 _userRequestId) {
+        _userRequestId =
+            COORDINATOR.requestRandomWords(keyHash, s_subscriptionId, requestConfirmations, callbackGasLimit, numWords);
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
 }
